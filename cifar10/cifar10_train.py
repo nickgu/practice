07dd27537ddc0b8da0d79f031dd944d356d36e3d
nickgu <simiu_0609@163.com>
2016-10-28 18:55:11 +0800

    add some network expr for cifar10

--- cifar10/cifar10_train.py
+++ cifar10/cifar10_train.py
@@ -59,7 +59,7 @@ class Preprocessor:
 
 if __name__ == '__main__':
     model_path = sys.argv[1]
-    net = nnet_tf.ConfigNetwork('net.conf', 'cifar10_cnn3_fc')
+    net = nnet_tf.ConfigNetwork('net.conf', 'cifar10_cnn3_fc_use_residual')
 
     temp_data = pydev.TempStorage('normal_data', 'temp/normal_data.ts')
     if temp_data.has_data():
