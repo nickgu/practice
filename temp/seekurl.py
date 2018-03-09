9858ccc5d8a47793a80c9e53639fdd6ac8dc50e2
斯缪 <simiu0609@gmail.com>
2018-03-10 02:36:48 +0800

    Update seekurl.py

--- temp/seekurl.py
+++ temp/seekurl.py
@@ -2,13 +2,16 @@ import sys
 import urllib2
 
 if __name__ == '__main__':
-		idx = 0
-		for url in sys.stdin.readlines():
-				url = url.strip()
-				fd = urllib2.urlopen(url)
-				data = fd.read()
-				outfile = file('output/%s.html' % idx, 'w')
-				outfile.write(data)
-				print 'complete [%d] [%s] len=%d' % (idx, url, len(data))
-				idx += 1
+	idx = 0
+	for url in sys.stdin.readlines():
+		url = url.strip()
+		try:
+			fd = urllib2.urlopen(url)
+			data = fd.read()
+			outfile = file('output/%s.html' % idx, 'w')
+			outfile.write(data)
+			print 'complete [%d] [%s] len=%d' % (idx, url, len(data))
+		except:
+			print 'error [%d] [%s]' % (idx, url)
+		idx += 1
 
