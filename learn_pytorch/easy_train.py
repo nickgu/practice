a6401679ecd6604883f014cfd8a14d39702006e8
nickgu <simiu_0609@163.com>
2019-01-03 10:55:37 +0800

    change code

--- learn_pytorch/easy_train.py
+++ learn_pytorch/easy_train.py
@@ -35,7 +35,7 @@ class IndexCoder:
 
     def save(self, fd):
         for value in self.tags:
-            fd.write('%s' % value)
+            fd.write('%s\n' % value)
 
     def load(self, fd):
         self.tags = []
@@ -44,6 +44,69 @@ class IndexCoder:
             tag = tag.strip()
             self.index[tag] = len(self.index)
             self.tags.append(tag)
+
+'''
+    Usage:
+        # pre-scan:
+        for ins:
+            for feature in ins:
+                slot_index_coder.alloc(slot_id, key)
+        # trainning.
+        for ins:
+            for feature in ins:
+                input.append(slot_index_coder.get(slot_id, key))
+'''
+class SlotIndexCoder:
+    def __init__(self):
+        self.__slot_index = {}
+
+    def get(self, slot, key):
+        if slot not in self.__slot_index:
+            return None
+        slot_coder = self.__slot_index[slot]
+        return slot_coder.get(key)
+
+    def alloc(self, slot, key):
+        if slot not in self.__slot_index:
+            self.__slot_index[slot] = IndexCoder()
+        return self.__slot_index[slot].alloc(key)
+
+    def save(self, fd):
+        fd.write('%s\n' % '\t'.join(self.__slot_index.keys()))
+        for slot, index_coder in self.__slot_index.iteritems():
+            for idx, value in enumerate(index_coder.tags):
+                fd.write('%s\t%s\t%d' % (slot, value, ))
+
+    def load(self, fd):
+        self.__slot_index = {}
+        slot_info = fd.readline().strip().split('\t')
+        for slot in slot_info:
+            self.__slot_index[slot] = IndexCoder()
+        pydev.info('%d slot info loaded' % len(self.__slot_index))
+        
+        for slot, key, idx in pydev.foreach_row(fd):
+            slot_index = self.__slot_index.get(slot, None)
+            if slot_index is None:
+                raise Exception('Cannot get slot : %s' % slot)
+
+            if int(idx) != len(slot_index.tags):
+                raise Exception('Index not match : %s:%s:%s' % (slot, idx, key))
+
+            slot_index.index[key] = len(slot_index.tags)
+            slot_index.tags.append(key)
+
+    def __equal__(self, peer):
+        if self.__slot_index.keys() != peer.__slot_index.keys():
+            return False
+
+        for slot, index in self.__slot_index.iteritems():
+            peer_index = peer.__slot_index[slot]
+            if peer_index.tags != self.tags:
+                return False
+            if peer_index.index != self.index:
+                return False
+
+        return True
 
 def dump_embeddings(emb, fd):
     for emb in emb.weight:
