25ff7c57783b877f995640a4dce23da411820ffc
nickgu <simiu_0609@163.com>
2019-01-06 11:25:00 +0800

    change code

---
+++ movielens/ranking/join_data.py
@@ -0,0 +1,87 @@
+#! /bin/env python
+# encoding=utf-8
+# author: nickgu 
+# 
+
+import tqdm
+
+import pydev
+import utils
+
+import easy.learn_utils as lu
+import easy.slot_file as sf
+
+class MovieLensRankingFeatureExtractor:
+    def __init__(self, movie_dir):
+        self.movies = utils.load_movies(movie_dir)
+        self.coder = lu.SlotIndexCoder()
+
+    def begin(self, uid, iid):
+        self.uid = int(uid)
+        self.iid = int(iid)
+        self.movie = self.movies.get(self.iid, None)
+
+    def processes(self):
+        return (self.f_uid, self.f_iid, self.f_user_genres, self.f_user_tag)
+
+    def f_uid(self):
+        return 'uid', [ self.uid ]
+
+    def f_iid(self):
+        return 'iid', [ self.iid ]
+
+    def f_user_genres(self):
+        slot = 'u_g'
+        lst = []
+        for genres in self.movie.genres:
+            key='%s_%s' % (self.uid, genres)
+            idx = self.coder.alloc(slot, key)
+            lst.append( idx )
+        return slot, lst
+
+    def f_user_tag(self):
+        slot = 'u_t'
+        lst = []
+        for tid, tag, score in self.movie.tags:
+            if score < 0.3:
+                continue
+            key='%s_%s' % (self.uid, tag)
+            idx = self.coder.alloc(slot, key)
+            lst.append( idx )
+        return slot, lst
+
+
+class JoinDataApp(pydev.App):
+    def __init__(self):
+        pydev.App.__init__(self)
+
+    def join(self):
+        arg = pydev.AutoArg()
+        test_num = int(arg.option('testnum', -1))
+        input_filename = arg.option('f')
+        movie_dir = arg.option('m')
+        output_filename = arg.option('o')
+        coder_output_filename = arg.option('c')
+        
+        data = utils.readfile(file(input_filename), test_num=test_num)
+
+        extractor = MovieLensRankingFeatureExtractor(movie_dir)
+        writer = sf.SlotFileWriter(output_filename)
+        for user_id, item_id, click in tqdm.tqdm(data):
+            writer.begin_instance(click)
+
+            extractor.begin(user_id, item_id)
+            ps = extractor.processes()
+            for p in ps:
+                slot, lst = p()
+                writer.write_slot(slot, lst)
+
+            writer.end_instance()
+
+        writer.summary()
+        
+
+
+if __name__=='__main__':
+    app = JoinDataApp()
+    app.run()
--- movielens/ranking/train_slot_lr.py
+++ movielens/ranking/train_slot_lr.py
@@ -20,7 +20,7 @@ import tqdm
 import numpy as np
 
 class SlotLRRank(nn.Module):
-    def __init__(self, user_count, item_count, user_genres_count, user_tags_count, embedding_size):
+    def __init__(self, user_count, item_count, user_genres_count, embedding_size):
         # Ranking model:
         # input emb_size * 2 (embbag of input, emb of item to predict)
         # fc x 4
@@ -29,34 +29,31 @@ class SlotLRRank(nn.Module):
         pydev.info('user_count=%d' % user_count)
         pydev.info('item_count=%d' % item_count)
         pydev.info('user_genres_count=%d' % user_genres_count)
-        pydev.info('user_tags_count=%d' % user_tags_count)
         pydev.info('embedding=%d' % embedding_size)
 
         self.uid_emb = nn.Embedding(user_count, embedding_size)
         self.iid_emb = nn.Embedding(item_count, embedding_size)
         self.user_genres_emb = nn.EmbeddingBag(user_genres_count, embedding_size, mode='mean')
-        self.user_tags_emb = nn.EmbeddingBag(user_tags_count, embedding_size, mode='mean')
 
         self.lr = nn.Linear(embedding_size*3, 1)
 
-    def forward(self, uid, iid, user_genres, user_genres_offset, user_tags, user_tags_offset):
+    def forward(self, uid, iid, user_genres, user_genres_offset):
         # sum up embeddings.
         user_emb = self.uid_emb(uid)
         item_emb = self.iid_emb(iid)
         user_genres_emb = self.user_genres_emb(user_genres, user_genres_offset)
-        user_tags_emb = self.user_tags_emb(user_tags, user_tags_offset)
 
-        x_ = torch.cat((user_emb, item_emb, user_genres_emb, user_tags_emb), 1)
+        x_ = torch.cat((user_emb, item_emb, user_genres_emb), 1)
         y = F.sigmoid( self.lr(x_) )
         return y
 
 class DataGenerator:
-    def __init__(self, train, device, epoch_count, batch_size):
+    def __init__(self, train, device, epoch_count, batch_size, movie_dir):
         max_movie_id = 0
         max_user_id = 0
 
         pydev.info('load movies')
-        self.movies = utils.load_movies('data/ml-20m_all', ignore_tags=False)
+        self.movies = utils.load_movies(movie_dir, ignore_tags=True)
 
         self.epoch_count = epoch_count
         self.batch_size = batch_size
@@ -80,15 +77,7 @@ class DataGenerator:
                 idx = self.slot_coder.alloc('uid_genres', key)
                 user_genres.append( idx )
 
-            user_tags = []
-            for tag_id, tag, score in movie.tags:
-                if score < 0.1:
-                    continue
-                key='%s_%s' % (uid, tag)
-                idx = self.slot_coder.alloc('uid_tag', key)
-                #user_tags.append( idx )
-
-            self.data.append( (uid, iid, user_genres, user_tags, click) )
+            self.data.append( (uid, iid, user_genres, click) )
 
         self.train_iter_count = self.epoch_count * self.data_count / self.batch_size
 
@@ -109,20 +98,15 @@ class DataGenerator:
             user_genres_ids = []
             user_genres_offset = []
 
-            user_tags_ids = []
-            user_tags_offset = []
             clicks = []
 
             epoch_num += 1
             pydev.info('Epoch %d' % epoch_num)
-            for uid, iid, user_genres, user_tags, click in self.data:
+            for uid, iid, user_genres, click in self.data:
                 user_ids.append(uid)
                 item_ids.append(iid)
                 user_genres_offset.append( len(user_genres_ids) )
                 user_genres_ids += user_genres
-
-                user_tags_offset.append( len(user_tags_ids) )
-                user_tags_ids += user_tags
 
                 clicks.append(float(click))
 
@@ -131,8 +115,6 @@ class DataGenerator:
                             torch.tensor(item_ids).to(self.device), 
                             torch.tensor(user_genres_ids).to(self.device), 
                             torch.tensor(user_genres_offset).to(self.device), 
-                            torch.tensor(user_tags_ids).to(self.device), 
-                            torch.tensor(user_tags_offset).to(self.device), 
                             torch.tensor(clicks).to(self.device))
 
                     user_ids = []
@@ -140,13 +122,11 @@ class DataGenerator:
                     clicks = []
                     user_genres_ids = []
                     user_genres_offset = []
-                    user_tags_ids = []
-                    user_tags_offset = []
 
 
 if __name__=='__main__':
-    if len(sys.argv)!=3:
-        print >> sys.stderr, 'Usage:\ndnn.py <datadir> <model>'
+    if len(sys.argv)!=4:
+        print >> sys.stderr, 'Usage:\ndnn.py <datadir> <moviedir> <model>'
         sys.exit(-1)
 
     TestNum = -1
@@ -159,12 +139,12 @@ if __name__=='__main__':
     pydev.info('Epoch=%d' % EpochCount)
     pydev.info('BatchSize=%d' % BatchSize)
 
-
     data_dir = sys.argv[1]
-    model_save_path = sys.argv[2]
+    movie_dir = sys.argv[2]
+    model_save_path = sys.argv[3]
 
     train, valid, test = utils.readdata(data_dir, test_num=TestNum)
-    data = DataGenerator(train, device, epoch_count=EpochCount, batch_size=BatchSize)
+    data = DataGenerator(train, device, epoch_count=EpochCount, batch_size=BatchSize, movie_dir=movie_dir)
 
     pydev.info('save index (num=%d)' % len(data.idx_coder.tags))
     data.idx_coder.save(file('temp/user_genres.idx', 'w'))
@@ -180,17 +160,17 @@ if __name__=='__main__':
     test_y_ = []
 
     def fwbp():
-        user_ids, item_ids, user_genres_ids, user_genres_offset, user_tags_ids, user_tags_offset, clicks = generator.next()
+        user_ids, item_ids, user_genres_ids, user_genres_offset, clicks = generator.next()
 
         #print user_ids.size()
         #print item_ids.size()
         #print user_genres_ids.size()
         #print user_genres_offset.size()
-        clicks_ = model.forward(user_ids, item_ids, user_genres_ids, user_genres_offset, user_tags_id, user_tags_offset)
+        clicks_ = model.forward(user_ids, item_ids, user_genres_ids, user_genres_offset)
         loss = loss_fn(clicks_, clicks)
         loss.backward()
 
-        del user_ids, item_ids, user_genres_ids, user_genres_offset, user_tags_ids, user_tags_offset, clicks
+        del user_ids, item_ids, user_genres_ids, user_genres_offset, clicks
         return loss[0]
 
     pydev.info('Begin training..')
