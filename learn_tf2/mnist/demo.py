17661ea7d604b5d7b4037fe55bc68a0ec1fd9574
nickgu <simiu_0609@163.com>
2019-11-10 19:53:30 +0800

    add learn_tf2

---
+++ learn_tf2/mnist/demo.py
@@ -0,0 +1,107 @@
+#! /bin/env python
+# encoding=utf-8
+# author: nickgu 
+# 
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+class MyModel(Model):
+  def __init__(self):
+    super(MyModel, self).__init__()
+    self.conv1 = Conv2D(32, 3, activation='relu')
+    self.flatten = Flatten()
+    self.d1 = Dense(128, activation='relu')
+    self.d2 = Dense(10, activation='softmax')
+
+  def call(self, x):
+    x = self.conv1(x)
+    x = self.flatten(x)
+    x = self.d1(x)
+    return self.d2(x)
+
+if __name__=='__main__':
+    # Model, Loss, Optimizer, Data
+
+    # prepare data.
+    mnist = tf.keras.datasets.mnist
+
+    print('=== Load data ===')
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    print('=== Load data over ===')
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    # Add a channels dimension
+    x_train = x_train[..., tf.newaxis]
+    x_test = x_test[..., tf.newaxis]
+    
+    # prepare train and test.
+    train_ds = tf.data.Dataset.from_tensor_slices(
+                (x_train, y_train)).shuffle(10000).batch(32)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+    # make Model.
+    model = MyModel()
+
+    # make Loss
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+
+    # make Optimizer
+    optimizer = tf.keras.optimizers.Adam()
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+
+    @tf.function
+    def train_step(images, labels):
+        with tf.GradientTape() as tape:
+            predictions = model(images)
+            loss = loss_object(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(labels, predictions)
+
+    @tf.function
+    def test_step(images, labels):
+        predictions = model(images)
+        t_loss = loss_object(labels, predictions)
+
+        test_loss(t_loss)
+        test_accuracy(labels, predictions)
+
+    print('=== Ready to train! ===')
+    EPOCHS = 5
+    for epoch in range(EPOCHS):
+        print('Step1 ...')
+        for idx, (images, labels) in enumerate(train_ds):
+            sys.stdout.write('%c%d/10000' %(13, idx))
+            train_step(images, labels)
+        sys.stdout.write('\n')
+
+        for test_images, test_labels in test_ds:
+            test_step(test_images, test_labels)
+
+        template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+        print (template.format(epoch+1,
+                             train_loss.result(),
+                             train_accuracy.result()*100,
+                             test_loss.result(),
+                             test_accuracy.result()*100))
+
+
+
+
+
+
+
---
+++ learn_tf2/readme.rst
@@ -0,0 +1,6 @@
+## learning tensorflow 2.0
+
+
+# mnist
+from tutorials
+https://github.com/tensorflow/docs/blob/master/site/zh-cn/tutorials/quickstart/advanced.ipynb
