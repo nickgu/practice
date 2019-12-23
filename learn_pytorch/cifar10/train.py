fbc9adea1dc1079fb23cb0d8a02990cbed8fc0ce
nickgu <simiu_0609@163.com>
2019-12-23 09:54:19 +0800

    change

--- learn_pytorch/cifar10/train.py
+++ learn_pytorch/cifar10/train.py
@@ -9,10 +9,73 @@ import sys
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 from torchvision.transforms import *
 
-import models
+#import models
+
+
+class ConvBNReluPool(nn.Module):
+    def __init__(self, in_chan, out_chan, pool=True):
+        nn.Module.__init__(self)
+        self.conv = nn.Sequential(
+                nn.Conv2d(in_chan, out_chan, [3,3], padding=1),
+                nn.BatchNorm2d(out_chan)
+                )
+        self.maxpool = pool
+
+    def forward(self, input):
+        x = input
+        x = self.conv(x)
+        x = F.relu(x)
+        if self.maxpool:
+            x = F.max_pool2d(x, [2,2])
+        return x
+
+
+class ResConvBlock(nn.Module):
+    def __init__(self, chan):
+        nn.Module.__init__(self)
+
+        self.conv1 = nn.Sequential(
+                nn.Conv2d(chan, chan, [3,3], padding=1),
+                nn.BatchNorm2d(chan)
+                )
+        self.conv2 = nn.Sequential(
+                nn.Conv2d(chan, chan, [3,3], padding=1),
+                nn.BatchNorm2d(chan)
+                )
+
+
+    def forward(self, input):
+        x = input
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv1(x)
+        x = F.relu(x)
+        return input + x
+
+
+class TempModel(nn.Module):
+    def __init__(self):
+        nn.Module.__init__(self)
+        self.net = nn.Sequential(
+                ConvBNReluPool(3, 64),
+                ConvBNReluPool(64, 128, pool=True),
+                ResConvBlock(128),
+                ConvBNReluPool(128, 256, pool=True),
+                ConvBNReluPool(256, 512),
+                ResConvBlock(512)
+                )
+        self.fc = nn.Linear(2048, 10)
+
+    def forward(self, input):
+        x = input
+        x = self.net(x)
+        x = x.view((-1,2048))
+        x = self.fc(x)
+        return x
 
 if __name__=='__main__':
     arg = pydev.Arg('Cifar10 training program with pytorch.')
@@ -32,18 +95,27 @@ if __name__=='__main__':
 
     # make simple Model.
 
-    transform_ops = Compose([
+    train_transform = Compose([
         RandomCrop(32, padding=2), 
         RandomHorizontalFlip(),
         #RandomRotation(30),
-        ToTensor()])
-    train = torchvision.datasets.cifar.CIFAR10('../../dataset/', transform=transform_ops)
-    test =  torchvision.datasets.cifar.CIFAR10('../../dataset/', train=False, transform=transform_ops)
+        ToTensor(),
+        Normalize(mean=(125.31, 122.95, 113.87), std=(62.99, 62.09, 66.70))
+        ])
+    test_transform = Compose([
+        ToTensor(),
+        Normalize(mean=(125.31, 122.95, 113.87), std=(62.99, 62.09, 66.70))
+        ])
+
+    train = torchvision.datasets.cifar.CIFAR10('../../dataset/', transform=train_transform)
+    test =  torchvision.datasets.cifar.CIFAR10('../../dataset/', train=False, transform=test_transform)
 
     # train phase.
     #model = models.SimpleConvNet()
     #model = models.Stack5ConvNet()
-    model = models.Res9Net()
+    #model = models.Res9Net()
+    
+    model = TempModel()
 
     sys.path.append('../')
     import easy_train
