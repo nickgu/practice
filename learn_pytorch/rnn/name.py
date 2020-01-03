faa5b8e48d6c8220b13abd371b36839bed086841
nickgu <simiu_0609@163.com>
2020-01-03 08:46:01 +0800

    change

--- learn_pytorch/rnn/name.py
+++ learn_pytorch/rnn/name.py
@@ -8,6 +8,7 @@
 from __future__ import unicode_literals, print_function, division
 from io import open
 import glob
+import sys
 import os
 import unicodedata
 import string
@@ -19,10 +20,9 @@ import math
 import torch
 import torch.nn as nn
 
-all_letters = string.ascii_letters + " .,;'-"
-n_letters = len(all_letters) + 1 # Plus EOS marker
 
 def findFiles(path): return glob.glob(path)
+
 
 # Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
 def unicodeToAscii(s):
@@ -36,6 +36,33 @@ def unicodeToAscii(s):
 def readLines(filename):
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
     return [unicodeToAscii(line) for line in lines]
+
+
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters) + 1 # Plus EOS marker
+n_categories = 0
+
+category_lines = {}
+all_categories = []
+
+# Build the category_lines dictionary, a list of lines per category
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+if n_categories == 0:
+    raise RuntimeError('Data not found. Make sure that you downloaded data '
+        'from https://download.pytorch.org/tutorial/data.zip and extract it to '
+        'the current directory.')
+
+print('# categories:', n_categories, all_categories)
+print(unicodeToAscii("O'Néàl"))
+
+
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -110,7 +137,7 @@ def randomTrainingExample():
     target_line_tensor = targetTensor(line)
     return category_tensor, input_line_tensor, target_line_tensor
 
-def train(category_tensor, input_line_tensor, target_line_tensor, criterion, learning_rate):
+def train(category_tensor, input_line_tensor, target_line_tensor, criterion, learning_rate, rnn):
     target_line_tensor.unsqueeze_(-1)
     hidden = rnn.initHidden()
 
@@ -154,29 +181,10 @@ def phase_train():
     all_losses = []
     total_loss = 0 # Reset every plot_every iters
 
-    # Build the category_lines dictionary, a list of lines per category
-    category_lines = {}
-    all_categories = []
-    for filename in findFiles('data/names/*.txt'):
-        category = os.path.splitext(os.path.basename(filename))[0]
-        all_categories.append(category)
-        lines = readLines(filename)
-        category_lines[category] = lines
-
-    n_categories = len(all_categories)
-
-    if n_categories == 0:
-        raise RuntimeError('Data not found. Make sure that you downloaded data '
-            'from https://download.pytorch.org/tutorial/data.zip and extract it to '
-            'the current directory.')
-
-    print('# categories:', n_categories, all_categories)
-    print(unicodeToAscii("O'Néàl"))
-
     start = time.time()
 
     for iter in range(1, n_iters + 1):
-        output, loss = train(*randomTrainingExample(), criterion, learning_rate)
+        output, loss = train(*randomTrainingExample(), criterion=criterion, learning_rate=learning_rate, rnn=rnn)
         total_loss += loss
 
         if iter % print_every == 0:
