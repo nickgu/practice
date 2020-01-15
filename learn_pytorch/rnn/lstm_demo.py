410f49b0aa46c03c3c61d8f9e48190e192a3a912
nickgu <simiu_0609@163.com>
2020-01-15 16:23:39 +0800

    change code

--- learn_pytorch/rnn/lstm_demo.py
+++ learn_pytorch/rnn/lstm_demo.py
@@ -21,10 +21,13 @@ class RNN(torch.nn.Module):
 
         self.__emb = torch.nn.Embedding(vocab_size, emb_size)
         self.__rnn = torch.nn.LSTM(emb_size, hidden_size, num_layers=self.__layer_num, batch_first=True)
+        self.__rnn.cuda()
         self.__fc = torch.nn.Linear(self.__hidden_size, self.__class_num)
+        self.__fc.cuda()
 
     def forward(self, x_id):
         emb = self.__emb(x_id)
+        emb = emb.cuda()
         output, _ = self.__rnn(emb)
         return self.__fc(output)
 
@@ -40,7 +43,12 @@ if __name__=='__main__':
     '''
     cuda = torch.device('cuda')     # Default CUDA device
     
-    epoch_count = 200
+    if len(sys.argv)<2:
+        print '%s <epoch_count>' % (sys.argv[0])
+        sys.exit(-1)
+
+    epoch_count = int(sys.argv[1])
+    print 'epoch=%d' % epoch_count
     data_size = 500
     batch_size = 16
     length = 64
@@ -74,8 +82,9 @@ if __name__=='__main__':
         bar = tqdm.tqdm(data_loader)
         for x, y in bar:
             optimizer.zero_grad()
-            y = model(train_x)
-            loss = criterion(y.view(-1,2), train_y.view(-1))
+            y = y.cuda()
+            y_ = model(x)
+            loss = criterion(y_.view(-1,2), y.view(-1))
             loss.backward()
             all_loss += loss
             batch_count += 1
@@ -83,27 +92,43 @@ if __name__=='__main__':
             optimizer.step()
 
     # test code.
-    count = 0
-    correct = 0
-    error = 0
-    for test_count in range(100):
-        x = torch.randint(vocab_size, [1, length])
-        y = model(x).softmax(dim=2).max(dim=2).indices
-        print x.tolist()
-        print y.tolist()
-        last = -1
-        for a, b in zip(x.view(-1).tolist(), y.view(-1).tolist()):
-            if a == last:
-                if b == 1: 
-                    correct+=1
-                count += 1
-            else:
-                if b == 1:
-                    error+=1
-            last = a
+    with torch.no_grad():
+        # test original
+        # seems that 30 epoch can make convergence (Adam(0.01))
+        train_y_ = model(train_x)
+        train_y_ = train_y_.view(-1,2).max(dim=1).indices
+        pred_label_pair = zip(train_y_.tolist(), train_y.view(-1).tolist())
 
-    print count, correct, error
-    print 'P=%.2f%%, R=%.2f%%' % (correct*100./(correct+error), correct*100./count)
+        count = len(filter(lambda x:x[1]==1, pred_label_pair))
+        correct = len(filter(lambda x:(x[0]==x[1] and x[1]==1), pred_label_pair))
+        error = len(filter(lambda x:(x[0]==1 and x[1]==0), pred_label_pair))
+        print 'train_data:'
+        print count, correct, error
+        print 'P=%.2f%%, R=%.2f%%' % (correct*100./(correct+error+1e-5), correct*100./count)
+
+        count = 0
+        correct = 0
+        error = 0
+        # test random.
+        for test_count in range(100):
+            x = torch.randint(vocab_size, [1, length])
+            y = model(x).softmax(dim=2).max(dim=2).indices
+            #print x.tolist()
+            #print y.tolist()
+            last = -1
+            for a, b in zip(x.view(-1).tolist(), y.view(-1).tolist()):
+                if a == last:
+                    if b == 1: 
+                        correct+=1
+                    count += 1
+                else:
+                    if b == 1:
+                        error+=1
+                last = a
+
+        print 'random_data:'
+        print count, correct, error
+        print 'P=%.2f%%, R=%.2f%%' % (correct*100./(correct+error+1e-5), correct*100./count)
 
 
 
